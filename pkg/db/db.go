f5b54bdbb5f75554b18205371097a55b7d044de5
asanliB <baltashovasanali@gmail.com>
2025-07-03 14:14:45 +0500

    Added interfaces and implemented methods for Redis and PostgreSQL
    - Introduced a common Pinger interface with the method Ping(ctx context.Context) error.
    - Implemented the Pinger interface for both PostgreSQL (pg) and Redis (redisDb).
    - For PostgreSQL: implemented ScanOneContext, ScanAllContext, ExecContext, QueryContext, QueryRowContext, BeginTx, Ping, and Close methods.
    - For Redis: implemented Set, Get, Del, Exists, Ping, and Close methods.
    - Created unified DB and RedisDB interfaces that include Pinger and Close, allowing for abstraction over database types.
    - Removed unnecessary checks and simplified logic in ScanAllContext.
    - Ensured consistent Ping behavior across different databases.

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,69 @@
 package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+type Handler func(ctx context.Context) error
+
+type Client interface {
+	DB() DB
+	Close() error
+	RedisDB() RedisDB
+}
+
+type TxManager interface {
+	ReadCommitted(ctx context.Context, f Handler) error
+}
+
+type Query struct {
+	QueryRaw string
+}
+
+type Transactor interface {
+	BeginTx(ctx context.Context, txOptioin pgx.TxOptions) (pgx.Tx, error)
+}
+
+type SQLExecer interface {
+	NamedExecer
+	QueryExecer
+}
+
+type NamedExecer interface {
+	ScanOneContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error
+	ScanAllContext(ctx context.Context, dest *[][]interface{}, q Query, args ...interface{}) error
+}
+
+type QueryExecer interface {
+	ExecContext(ctx context.Context, q Query, args ...interface{}) (pgconn.CommandTag, error)
+	QueryContext(ctx context.Context, q Query, args ...interface{}) (pgx.Rows, error)
+	QueryRowContext(ctx context.Context, q Query, args ...interface{}) pgx.Row
+}
+
+type Redis interface {
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+	Del(ctx context.Context, key string) error
+	Exists(ctx context.Context, key string) (bool, error)
+}
+
+type Pinger interface {
+	Ping(ctx context.Context) error
+}
+
+type DB interface {
+	SQLExecer
+	Transactor
+	Pinger
+	Close()
+}
+
+type RedisDB interface {
+	Redis
+	Pinger
+	Close()
+}
---
+++ pkg/db/redise/redis.go
@@ -0,0 +1,69 @@
+package redise
+
+import (
+	"app/market/pkg/db"
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// type Redis interface {
+// 	Set(ctx context.Context, key string, value interface{}) error
+// 	Get(ctx context.Context, key string) (string, error)
+// 	Del(ctx context.Context, key string) error
+// 	Exists(ctx context.Context, key string) (bool, error)
+// }
+
+type redisDb struct {
+	redisDb *redis.Client
+}
+
+func NewRedise(rDB *redis.Client) db.RedisDB {
+	return &redisDb{
+		redisDb: rDB,
+	}
+}
+
+func (r *redisDb) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+
+	return r.redisDb.Set(ctx, key, value, expiration).Err()
+}
+
+func (r *redisDb) Get(ctx context.Context, key string) (string, error) {
+	result, err := r.redisDb.Get(ctx, key).Result()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+
+}
+func (r *redisDb) Del(ctx context.Context, key string) error {
+	_, err := r.redisDb.Del(ctx, key).Result()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+func (r *redisDb) Exists(ctx context.Context, key string) (bool, error) {
+	result, err := r.redisDb.Exists(ctx, key).Result()
+
+	if err != nil {
+		return false, err
+	}
+
+	return result == 1, nil
+}
+
+func (r *redisDb) Ping(ctx context.Context) error {
+	return r.redisDb.Ping(ctx).Err()
+}
+
+func (r *redisDb) Close() {
+	r.redisDb.Close()
+}
